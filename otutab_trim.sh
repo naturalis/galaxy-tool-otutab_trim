0ddf526e021ad8d683c245a40f929b131b21c2ef
MartenHoogeveen <martenhoogeveen@gmail.com>
2018-10-15 14:34:03 +0200

    Added param

--- otutab_trim.sh
+++ otutab_trim.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-usearch10.0.240 -otutab_trim $1 -min_freq $2 -output $3 > /dev/null 2> $4
+usearch10.0.240 -otutab_trim $1 -min_sample_size $2 -min_count $3 -min_freq $4 -min_otu_size $5 -min_otu_freq $6 -output $7 > /dev/null 2> $8
