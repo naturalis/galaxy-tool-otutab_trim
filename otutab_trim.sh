1af9032c48ccbdadaffe5709d9d75b5f59ef6b30
MartenHoogeveen <martenhoogeveen@gmail.com>
2019-04-08 13:43:25 +0000

    update

--- otutab_trim.sh
+++ otutab_trim.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-usearch10.0.240 -otutab_trim $1 -min_sample_size $2 -min_count $3 -min_freq $4 -min_otu_size $5 -min_otu_freq $6 -output $7 > /dev/null 2> $8
+usearch11 -otutab_trim $1 -min_sample_size $2 -min_count $3 -min_freq $4 -min_otu_size $5 -min_otu_freq $6 -output $7 > /dev/null 2> $8
